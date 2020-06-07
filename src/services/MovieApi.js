40c4c440c89ae2abec7f4817e7f507001f8a2de8
poojadas07 <b418033@iiit-bh.ac.in>
2020-06-07 17:11:52 +0530

    refactoring the Axios/API Part

--- src/services/MovieApi.js
+++ src/services/MovieApi.js
@@ -3,14 +3,14 @@ import axios from 'axios'
 export default {
 
   fetchMovieCollection (name) {
-    return axios.get('&s=' + name)
+    return axios.get('&s='+name)
       .then(response => {
         return response.data
       })
   },
 
   fetchSingleMovie (id) {
-    return axios.get('&i=' + id)
+    return axios.get('&i='+id)
       .then(response => {
         return response.data
       })
