e4b883ed1e7609a867f71ecdee8d02837f6a927a
poojadas07 <b418033@iiit-bh.ac.in>
2020-06-07 13:57:18 +0530

    Movie App

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from "vue";
 import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import "vuetify/dist/vuetify.min.css";
+import router from "./router";
+import axios from "axios";
+import { store } from "./store";
+
+Vue.use(vuetify);
 
 Vue.config.productionTip = false;
 
+axios.defaults.baseURL = "http://www.omdbapi.com/?apikey=e0620bd4&page=1&type=movie&Content-Type=application/json";
+
 new Vue({
-  render: h => h(App)
+  vuetify,
+  render: h => h(App),
+  store,
+  router
 }).$mount("#app");
